7269e9473b0c7c54203cff80fcac71ebbe727392
soft-TOP <TorstenPohling@soft-TOP.de>
2016-10-24 10:16:14 +0200

    neues Beispiel

---
+++ Erweiterungsklasse/ExtString.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erweiterungsklasse
+{
+    public static class ExtString
+    {
+        /// <summary>
+        /// Die Zeichen, welche als Trennzeichen zwischen Vornamen und Nachnamen angenommen werden
+        /// </summary>
+        static private Char[] chars = new Char[] { ' ', '.', ',', ':', '-' };
+
+        /// <summary>
+        /// Trennt einen übergebenen Namen in Vornamen und Nachnamen
+        /// </summary>
+        /// <param name="Name">Der zu trennende Name</param>
+        /// <returns>Vorname</returns>
+        public static String Vorname(this String Name)
+        {
+            return Name.Substring(0, Name.IndexOfAny(chars));
+        }
+
+        /// <summary>
+        /// Trennt einen übergebenen Namen in Vornamen und Nachnamen
+        /// </summary>
+        /// <param name="Name">Der zu trennende Name</param>
+        /// <param name="Trennzeichen">Das Trennzeichen zwischen den Namen</param>
+        /// <returns>Vorname</returns>
+        public static String Vorname(this String Name, Char Trennzeichen)
+        {
+            return Name.Substring(0, Name.IndexOf(Trennzeichen));
+        }
+
+        /// <summary>
+        /// Trennt einen übergebenen Namen in Vornamen und Nachnamen
+        /// </summary>
+        /// <param name="Name">Der zu trennende Name</param>
+        /// <returns>Nachname</returns>
+        public static String Nachname(this String Name)
+        {
+            return Name.Substring(Name.IndexOfAny(chars)+1);
+        }
+
+        /// <summary>
+        /// Trennt einen übergebenen Namen in Vornamen und Nachnamen
+        /// </summary>
+        /// <param name="Name">Der zu trennende Name</param>
+        /// <param name="Trennzeichen">Das Trennzeichen zwischen den Namen</param>
+        /// <returns>Nachname</returns>
+        public static String Nachname(this String Name, Char Trennzeichen)
+        {
+            return Name.Substring(Name.IndexOf(Trennzeichen)+1);
+        }
+    }
+}
--- Erweiterungsklasse/Program.cs
+++ Erweiterungsklasse/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Erweiterungsklasse;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,17 @@ namespace Erweiterungsklasse
     {
         static void Main(string[] args)
         {
+            String Name1 = "Torsten Pohling";
+            String Name2 = "Torsten-Pohling";
+            String Name3 = "Torsten:Pohling";
+            String Name4 = "Torsten#Pohling";
+
+            Console.WriteLine($"Vorname: {Name1.Vorname()}; Nachname: { Name1.Nachname()}");
+            Console.WriteLine($"Vorname: {Name2.Vorname()}; Nachname: { Name2.Nachname()}");
+            Console.WriteLine($"Vorname: {Name3.Vorname()}; Nachname: { Name3.Nachname()}");
+            Console.WriteLine($"Vorname: {Name4.Vorname('#')}; Nachname: { Name4.Nachname('#')}");
+
+            Console.ReadLine();
         }
     }
 }
